2cb7c42d373e52fbb38b060f71255129979978cf
AnReys <90377104+AnReys@users.noreply.github.com>
2021-10-07 21:53:54 +0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,10 @@
+function setheight(){
+  var clientHeight = document.body.clientHeight;
+  $('.hei').height(clientHeight);
+}
+$(document).ready(function() {
+   function setheight();
+});
+$(window).resize(function(){
+   function setheight();
+});
